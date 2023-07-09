60397845b8f957b33ad7009df80c1af91e7c16fb
Benjote <benjote014@gmail.com>
2023-07-09 17:02:07 -0400

    2.9
    Cueck

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -7,33 +7,65 @@ public class Dialogue : MonoBehaviour
     [SerializeField] private GameObject dialogueMark;
     [SerializeField] private GameObject dialoguePanel;
     [SerializeField] private TMP_Text dialogueText;
-    [SerializeField, TextArea(4, 6)] private string[] dialogueLines;
+    [SerializeField, TextArea(4, 6  )] private string[] dialogueLines;
 
     private float typingTime = 0.05f;
+
+
 
     private bool isPlayerInRange;
     private bool didDialogueStart;
     private int lineIndex;
 
+   
     private void Update()
     {
-        if (isPlayerInRange && Input.GetButtonDown("Fire1"))
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.Return))
         {
             if (!didDialogueStart)
             {
                 StartDialogue();
             }
+            else if(dialogueText.text == dialogueLines[lineIndex])
+            {
+                NextDialogueLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                dialogueText.text = dialogueLines[lineIndex];
+            }
+                
+
         }
     }
 
-    private void StartDialogue()
-    {
+
+   private void StartDialogue()
+   {
         didDialogueStart = true;
         dialoguePanel.SetActive(true);
         dialogueMark.SetActive(false);
         lineIndex = 0;
+        Time.timeScale = 0f;
         StartCoroutine(ShowLine());
-    }
+   }
+
+   private void NextDialogueLine()
+   {
+        lineIndex++;
+        if(lineIndex < dialogueLines.Length)
+        {
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            didDialogueStart = false;
+            dialoguePanel.SetActive(false);
+            dialogueMark.SetActive(true);
+            Time.timeScale = 1F;
+        }
+   }
 
     private IEnumerator ShowLine()
     {
@@ -45,13 +77,15 @@ public class Dialogue : MonoBehaviour
             yield return new WaitForSeconds(typingTime);
         }
     }
-
+        
+  
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             isPlayerInRange = true;
             dialogueMark.SetActive(true);
+            Debug.Log("Se puede iniciar un diálogo");
         }
     }
 
@@ -61,6 +95,7 @@ public class Dialogue : MonoBehaviour
         {
             isPlayerInRange = false;
             dialogueMark.SetActive(false);
+            Debug.Log("No se puede iniciar un diálogo");
         }
     }
 }
