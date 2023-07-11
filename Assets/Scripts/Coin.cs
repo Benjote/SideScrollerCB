3c2f34a85ec8df2b457ac6053ae8a6687bb5b3bd
Benjote <benjote014@gmail.com>
2023-07-11 00:06:53 -0400

    3.1
    Monedas, falta agregar el guardado de datos entre niveles.

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int valor = 1;
+    public GameManager gameManager;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            gameManager.SumarPuntos(valor);
+            Destroy(this.gameObject);
+        }
+    }
+}
